2e93a3ef524d4255fa2935392767914c50cb2dd4
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-09 16:26:58 -0300

    Updated on EmailService

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,3 +1,4 @@
+// interfaces defines our custom types and the type of yours attributes.
 interface IMailTo {
   name: string;
   email: string;
@@ -6,15 +7,21 @@ interface IMailTo {
 interface IMailMessage {
   subject: string;
   body: string;
-  attachament?: Array<string>;
+  attachament?: Array<string>; // The ? operator indicates not-required information.
 }
 
+// Data-transfer-object to transfer information between two different files in our application.
 interface MessageDTO {
   to: IMailTo;
   message: IMailMessage;
 }
 
-class EmailService {
+// This interface determines the standard model of our EmailService's class.
+interface IEmailService {
+  send(request: MessageDTO): void
+}
+
+class EmailService implements IEmailService {
   send({ to, message }: MessageDTO) {
     console.log(`Email sent to ${to.email}: ${message.subject}`);
   }
